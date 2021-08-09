ad91aaf716b51be21f6cd508a301ac097c81e3b7
QiLin <qq512554339@gmail.com>
2021-08-09 19:18:28 +0800

    修复获取m3u8链接失败的问题

--- downloadm3u8.py
+++ downloadm3u8.py
@@ -18,15 +18,15 @@ with open(os.getcwd()+"\\url.txt", "r",encoding='utf-8') as urls:
             f.write(response.text)
         with open(os.getcwd()+"\\temp.txt", "r",encoding='utf-8') as f:
             for line in f.readlines():
-                if line.find('<title>')==-1:
+                if line.find('<title> ')==-1:
                     pass
                 else:  
-                    title=line.replace('<title>','').replace(' ','')                
-                if line.find('img2')==-1:
+                    title=line.replace('<title>','').replace(' ','')                  
+                if line.find('<div id=VID style="display:none;">')==-1:
                     pass
                 else:
-                    m3u8=line.replace('<video id="player_one" class="video-js vjs-sublime-skin vjs-16-9" preload="auto" width="100%"  controls poster="https://img2.t6k.co/thumb/','').replace('.jpg" data-setup=\'{}\'>','').replace(" ","").replace("\t","").strip()
-                    m3u8url='https://ccn.killcovid2021.com//m3u8/'+m3u8+'/'+m3u8+'.m3u8'
+                    m3u8=line.replace('<div id=VID style="display:none;">','').replace('</div>','').replace(" ","").replace('\t','').strip()
+                    m3u8url='https://ccn.killcovid2021.com/m3u8/'+m3u8+'/'+m3u8+'.m3u8'
                     with open(os.getcwd()+"\\m3u8url.txt","a",encoding='utf-8') as f:
                         f.write(m3u8url+','+title)
     print("所有视频下载地址获取完成，接下来可以把存在m3u8url.txt中的结果复制到M3U8Dwonloader中下载了")
